3d3937ce8ae4b6a8dd8afe88da79296b7c2f53c7
ameertheein <ameertheein.k@gmail.com>
2024-06-24 14:57:31 +0530

    Revert "first commit"
    This reverts commit ca053f48ca092cc0d53a4a4255d2c72a4c0f0096.

--- src/main/java/com/example/todo/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.todo.enums;
-
-public enum Gender {
-    MALE,FEMALE
-}
--- src/main/java/com/example/todo/enums/Role.java
+++
@@ -1,8 +0,0 @@
-package com.example.todo.enums;
-
-public enum Role {
-    ADMIN,
-    SUPERVISOR,
-    AGENT
-
-}
